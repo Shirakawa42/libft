3c1c9a675d147fddcfddacf184276e439a1f45dd
Lucas VASSEUR <lvasseur@e2r9p11.42.fr>
2016-11-21 12:00:42 +0100

    yeah

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: lvasseur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 13:21:31 by lvasseur          #+#    #+#             */
-/*   Updated: 2016/11/13 14:39:27 by lvasseur         ###   ########.fr       */
+/*   Updated: 2016/11/21 12:00:19 by lvasseur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ size_t	ft_strlcat(char *dest, const char *src, size_t n)
 	i = 0;
 	while (j < n && dest[j])
 		j++;
-	while (j + i < n - 1 && src[i])
+	while (j + i < n - 1 && src[i] && n != 0)
 	{
 		dest[j + i] = src[i];
 		i++;
