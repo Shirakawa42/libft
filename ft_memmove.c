4ea50849294796ab52a75e80305cde5d87ddd5f6
Lucas VASSEUR <lvasseur@e2r9p11.42.fr>
2016-11-21 14:06:37 +0100

    yeah

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,21 +6,35 @@
 /*   By: lvasseur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 16:23:49 by lvasseur          #+#    #+#             */
-/*   Updated: 2016/11/21 13:45:01 by lvasseur         ###   ########.fr       */
+/*   Updated: 2016/11/21 14:04:31 by lvasseur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t len)
 {
-	char	*str;
+	char	tmp;
+	size_t	i;
+	char	*dest2;
+	char	*src2;
 
-	if ((str = (char*)malloc(sizeof(char) * len)) == 0)
-		return (NULL);
-	ft_memcpy(str, src, len);
-	ft_memcpy(dest, str, len);
-	free(str);
-	return (dest);
+	dest2 = (char*)dest;
+	src2 = (char*)src;
+	i = 0;
+	if (dest > src)
+	{
+		while (len)
+		{
+			len--;
+			dest2[len] = src2[len];
+		}
+	}
+	while (i < len)
+	{
+		tmp = src2[i];
+		dest2[i] = tmp;
+		i++;
+	}
+	return (dest2);
 }
