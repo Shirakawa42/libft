2550c5d561691f95edfa02e750bd64fb26890f3e
Lucas VASSEUR <lvasseur@e1r7p19.42.fr>
2018-05-24 13:49:56 +0200

    fix

--- ft_putaddr.c
+++ ft_putaddr.c
@@ -6,7 +6,7 @@
 /*   By: lvasseur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/09 14:32:28 by lvasseur          #+#    #+#             */
-/*   Updated: 2018/05/22 15:55:48 by lvasseur         ###   ########.fr       */
+/*   Updated: 2018/05/24 13:49:31 by lvasseur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	ft_putaddr(uint64_t addr)
 {
 	if (addr > 16)
 	{
-		ft_putaddr(addr / 16);
 		ft_putaddr(addr % 16);
+		ft_putaddr(addr / 16);
 	}
 	else if (addr < 10)
 		ft_putchar(addr + '0');
