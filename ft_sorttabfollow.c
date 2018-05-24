afa5c1a22153bca5467f090b0061414c06ac0e5f
Lucas VASSEUR <lvasseur@e1r7p19.42.fr>
2018-05-24 14:44:11 +0200

    sorttabfollower

--- ft_sorttabfollow.c
+++ ft_sorttabfollow.c
@@ -6,31 +6,30 @@
 /*   By: lvasseur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/24 14:31:24 by lvasseur          #+#    #+#             */
-/*   Updated: 2018/05/24 14:31:29 by lvasseur         ###   ########.fr       */
+/*   Updated: 2018/05/24 14:44:00 by lvasseur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char		**ft_sorttabfollow(char **tab, char **follower)
+void	ft_sorttabfollow(char ***tab, char ***follower)
 {
 	char	*tmp;
 	int		i;
 
 	i = 1;
-	while (tab[i])
+	while (*tab[i])
 	{
-		if (ft_strcmp(tab[i], tab[i - 1]) < 0)
+		if (ft_strcmp(*tab[i], *tab[i - 1]) < 0)
 		{
-			tmp = tab[i];
-			tab[i] = tab[i - 1];
-			tab[i - 1] = tmp;
-			tmp = follower[i];
-			follower[i] = follower[i - 1];
-			follower[i - 1] = tmp;
+			tmp = *tab[i];
+			*tab[i] = *tab[i - 1];
+			*tab[i - 1] = tmp;
+			tmp = *follower[i];
+			*follower[i] = *follower[i - 1];
+			*follower[i - 1] = tmp;
 			i = 0;
 		}
 		i++;
 	}
-	return (tab);
 }
