936e74e221dba103f59ddf84865e4060a14f2e29
Lucas VASSEUR <lvasseur@e1r5p11.42.fr>
2019-11-13 17:49:25 +0100

    order_tab_ascii && two_follow

--- ft_order_tab_by_ascii_two_follow.c
+++ ft_order_tab_by_ascii_two_follow.c
@@ -6,7 +6,7 @@
 /*   By: lvasseur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 17:27:07 by lvasseur          #+#    #+#             */
-/*   Updated: 2019/11/13 17:41:01 by lvasseur         ###   ########.fr       */
+/*   Updated: 2019/11/13 17:49:04 by lvasseur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ static void	moves(int t, int i, char ***tab)
 {
 	char	*tmp;
 
-	tmp = ft_strdup(*tab[t]);
-	free(*tab[t]);
-	*tab[t] = *tab[i];
-	*tab[i] = tmp;
+	tmp = ft_strdup((*tab)[t]);
+	free((*tab)[t]);
+	(*tab)[t] = (*tab)[i];
+	(*tab)[i] = tmp;
 }
 
 void		ft_order_tab_by_ascii_two_follow(char ***tab,
@@ -39,7 +39,7 @@ void		ft_order_tab_by_ascii_two_follow(char ***tab,
 		j = current_start - 1;
 		t = current_start;
 		while (++j < nbstr)
-			if (t != j && ft_strcmp(*tab[t], *tab[j]))
+			if (t != j && ft_strcmp((*tab)[t], (*tab)[j]))
 				t = j;
 		moves(t, i, tab);
 		moves(t, i, tabb);
